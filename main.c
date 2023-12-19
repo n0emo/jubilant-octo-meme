37b4249c874ca65531db082fc955275f76ef2ff5
Albert Shefner <solaris945@gmail.com>
2023-12-19 12:16:09 +0300

    Добавлено 3 число в сумму

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d\n", a + b);
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d\n", a + b + c);
 	return 0;
 }
